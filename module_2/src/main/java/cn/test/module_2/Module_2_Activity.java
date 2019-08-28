0be9b4c2c2f80928209e2c2d3de7c106fde78a22
刘庞 <liupang@liupangdeMacBook-Pro.local>
2019-08-28 22:36:34 +0800

    完成壳集成以及组件独立调试

---
+++ module_2/src/main/java/cn/test/module_2/Module_2_Activity.java
@@ -0,0 +1,13 @@
+package cn.test.module_2;
+
+import android.os.Bundle;
+
+import cn.test.base.BaseActivity;
+
+public class Module_2_Activity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_2_activity);
+    }
+}
